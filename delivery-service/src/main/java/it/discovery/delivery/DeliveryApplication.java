6ac1fa0b267f762f4e62ec4a47c772ae2f1eee04
Sergiy <sergey.morenets@gmail.com>
2023-11-04 11:55:45 +0200

    Task 1. Fix configuration & dependencies

--- delivery-service/src/main/java/it/discovery/delivery/DeliveryApplication.java
+++ delivery-service/src/main/java/it/discovery/delivery/DeliveryApplication.java
@@ -1,12 +1,26 @@
 package it.discovery.delivery;
 
+import it.discovery.delivery.domain.Manager;
+import it.discovery.delivery.persistence.ManagerRepository;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DeliveryApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DeliveryApplication.class, args);
+    }
+
+    @Bean
+    ApplicationRunner applicationRunner(ManagerRepository managerRepository) {
+        return args -> {
+            Manager manager = new Manager();
+            manager.setName("Peter");
+            manager.setPhone("123");
+            managerRepository.save(manager);
+        };
     }
 }
