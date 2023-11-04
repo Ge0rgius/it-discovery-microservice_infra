97abbc6d3a9d56e02b9f6b634f37f895e448bea7
Sergiy <sergey.morenets@gmail.com>
2023-11-04 15:57:45 +0200

    Task 5.2. Configure payment service as Eureka client

--- payment-service/src/main/java/it/discovery/payment/PaymentApplication.java
+++ payment-service/src/main/java/it/discovery/payment/PaymentApplication.java
@@ -9,10 +9,12 @@ import it.discovery.payment.persistence.PaymentProviderRepository;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class PaymentApplication {
 
     public static void main(String[] args) {
