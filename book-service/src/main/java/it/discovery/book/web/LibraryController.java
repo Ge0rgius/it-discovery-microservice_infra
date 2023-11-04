4dfa624cb3cb03de7336c85ecf0a1e8633bfcdd7
Sergiy <sergey.morenets@gmail.com>
2023-11-04 15:01:36 +0200

    Task 4

--- book-service/src/main/java/it/discovery/book/web/LibraryController.java
+++ book-service/src/main/java/it/discovery/book/web/LibraryController.java
@@ -1,7 +1,5 @@
 package it.discovery.book.web;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,11 +7,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping
 public class LibraryController {
 
-    @Value("${training}")
-    private String trainingName;
-
-    @GetMapping("training")
-    public String getTrainingName() {
-        return trainingName;
-    }
+//    @Value("${training}")
+//    private String trainingName;
+//
+//    @GetMapping("training")
+//    public String getTrainingName() {
+//        return trainingName;
+//    }
 }
